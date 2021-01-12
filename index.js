781bd7c356074da33d8998bd0731775acfbf5907
AAGaming00 <aa@bigdumb.gq>
2021-01-12 11:09:48 -0500

    fix more stuff

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ const { rename } = require('fs').promises
 const { Plugin } = require('@vizality/entities');
 const { patch, unpatch } = require('@vizality/patcher');
 const mod = require('module');
-const modulePath = join(__dirname, 'modules');
 const restoreLocalStorage = require('./localStorage');
 const toProxy = require('./toProxy');
 
 module.exports = class PCCompat extends Plugin {
   async onStart () {
-    
     if (!__dirname.endsWith('00pccompat')) {
       await rename(__dirname, join(__dirname, '..', '00pccompat'))
       window.location.reload()
@@ -25,32 +23,31 @@ module.exports = class PCCompat extends Plugin {
 
     this.path = join(__dirname, 'modules');
     mod.globalPaths.push(this.path);
-    window.powercord = {
-      ...vizality,
+    window.powercord = toProxy(vizality, {
       pluginManager: toProxy(vizality.manager.plugins, {
-        getPlugins: () => vizality.manager.plugins.plugins
+        getPlugins: () => vizality.manager.plugins.items,
+        get plugins() { return vizality.manager.plugins.items }
       }),
       styleManager: toProxy(vizality.manager.themes, {
-        getStyles: () => vizality.manager.themes.themes
+        getStyles: () => vizality.manager.themes.items,
+        get styles() { return vizality.manager.themes.items }
       }),
-      api: {
-        ...vizality.api,
+      api: toProxy(vizality.api, {
         settings: toProxy(vizality.api.settings, {
           registerSettings: (id, opts) => vizality.api.settings.registerAddonSettings({
             id,
             ...opts
           }),
           unregisterSettings: (id) => {
-            vizality.api.router.unregisterRoute(`/dashboard/plugins/${id}`);
-            vizality.api.settings.unregisterSettings(id);
+            vizality.api.settings.unregisterAddonSettings(id);
           }
         }),
         i18n: toProxy(vizality.api.i18n, {
           loadAllStrings: vizality.api.i18n.injectAllStrings,
           loadStrings: vizality.api.i18n.injectStrings
         })
-      }
-    };
+      })
+    });
 
     restoreLocalStorage();
 
@@ -78,9 +75,9 @@ module.exports = class PCCompat extends Plugin {
 
   onStop () {
     const tempDisabled = [];
-    vizality.manager.plugins.plugins.forEach((e, i) => {
+    vizality.manager.plugins.items.forEach((e, i) => {
       try {
-        const plugin = vizality.manager.plugins.plugins.get(i);
+        const plugin = vizality.manager.plugins.items.get(i);
         if (e.startPlugin) {
           if (vizality.manager.plugins.isEnabled(i)) {
             vizality.manager.plugins.disable(i);
@@ -101,7 +98,7 @@ module.exports = class PCCompat extends Plugin {
     unpatch('pcCompat-load');
     delete vizality.manager.plugins.mount;
     mod.globalPaths = mod.globalPaths.filter(x => x !== this.path);
-    Object.keys(require.cache).filter(x => x.includes('/modules/powercord')).forEach((x) => delete require.cache[x]);
+    Object.keys(require.cache).filter(x => x.includes('/modules/powercord'))?.forEach((x) => delete require.cache[x]);
     delete window.powercord;
     delete window.localStorage;
     this._fontAwesome.remove();
