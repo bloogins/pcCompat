5d1deb21a4d77ea9be3b04d55789fb0339b7bbee
AAGaming00 <aa@bigdumb.gq>
2021-01-12 11:12:55 -0500

    fixes

--- localStorage.js
+++ localStorage.js
@@ -1,4 +1,5 @@
 module.exports = () => {
+  if (window.localStorage)
   const frame = document.createElement('iframe');
   document.body.appendChild(frame);
   window.localStorage = Object.getOwnPropertyDescriptor(frame.contentWindow, 'localStorage').get.call(window);
