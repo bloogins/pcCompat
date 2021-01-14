c975942130715c9fe0173c789719e2b8f37119cf
AAGaming00 <aa@bigdumb.gq>
2021-01-14 10:14:20 -0500

    fix the dumb

--- modules/powercord/entities.js
+++ modules/powercord/entities.js
@@ -1,5 +1,5 @@
 const entities = require('@vizality/entities');
-const toProxy = require('../../toProxy')
+const toProxy = require('../../toProxy');
 
 class Plugin extends entities.Plugin {
     constructor (...props) {
@@ -7,9 +7,11 @@ class Plugin extends entities.Plugin {
         this._isPCCompat = true
         if (this.start !== Plugin.prototype.start) {
             this.__pcCompatStart = this.start
+            this.start = Plugin.prototype.start
         }
         if (this.stop !== Plugin.prototype.stop) {
             this.__pcCompatStop = this.stop
+            this.stop = Plugin.prototype.stop
         }
     }
     start (...args) {
