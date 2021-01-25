4ca47e43df35b390f9fb4858118421c7435c97b8
AAGaming00 <aa@bigdumb.gq>
2021-01-25 10:56:45 -0500

    icons but better

--- modules/powercord/components/Icons/index.js
+++ modules/powercord/components/Icons/index.js
@@ -1,10 +1,21 @@
-
 const React = require('react');
-const Icon = require('@vizality/components/Icon');
-const WrappedIcon = (props) => React.createElement(Icon, props);
-Icon.default.Names.forEach(name => WrappedIcon[name] = (props) => React.createElement(Icon, { name, ...props }));
+const Icon = require('@vizality/components/Icon').default;
 
-WrappedIcon.FontAwesome = require('./FontAwesome');
-WrappedIcon.badges = require('@vizality/components').Icon.badges;
+const fa = require('./FontAwesome');
+
+const WrappedIcon = new Proxy(Icon, {
+  get: (target, prop, receiver) => {
+    if (Reflect.has(target, prop)) {
+      return Reflect.get(target, prop, receiver);
+    }
+    if (prop === 'FontAwesome') return fa;
+    return (props) => React.createElement(Icon, { name: prop, ...props });
+  },
+  set: (target, prop, value) => {
+    return target[prop] = value;
+  }
+});
+
+// WrappedIcon.badges = require('@vizality/components').Icon.badges;
 
 module.exports = WrappedIcon;
--- toProxy.js
+++ toProxy.js
@@ -1,10 +1,10 @@
 module.exports = function toProxy (t, props) {
   return new Proxy(t, {
-    get: (target, prop) => {
+    get: (target, prop, receiver) => {
       if (Object.keys(props).includes(prop)) {
         return props[prop];
       }
-      return target[prop];
+      return Reflect.get(target, prop, receiver);
     },
     set: (target, prop, value) => {
       if (Object.keys(props).includes(prop)) {
